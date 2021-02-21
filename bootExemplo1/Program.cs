bd7b7c486ec85ae67e1dc063e8c908f1a7b920d6
hlbs <hermesbarros.eng@gmail.com>
2021-02-21 01:13:44 -0300

    first commit

---
+++ bootExemplo1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace bootExemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes =5;
+
+            for (int i=1; i<numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Me tornei autosuficiente! Você tem apenas {i} segundos..");
+            }
+        }
+    }
+}
